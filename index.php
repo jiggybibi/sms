ecffe24ccbd8e93427722550bb52e44d1e381489
jiggybibi <marlon28mcintosh@gmail.com>
2021-10-30 16:12:29 -0500

    created project and folder structure

---
+++ index.php
@@ -0,0 +1,8 @@
+    <?php 
+        
+        $title = 'Home';
+        require_once 'includes/header.php';
+    ?>
+        <h1>Hello, world!</h1>
+        
+    <?php require_once 'includes/footer.php' ?>
---
+++ lecturers/index.php
@@ -0,0 +1,8 @@
+<?php 
+        
+        $title = 'Lecturers Home';
+        require_once 'includes/header.php';
+    ?>
+        <h1>Hello, world!</h1>
+        
+    <?php require_once 'includes/footer.php' ?>
